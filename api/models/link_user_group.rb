290bf794d768c825c1fa01fab1454426b78d110d
Aaron Parecki <aaron@parecki.com>
2013-06-19 22:43:29 -0700

    Moved API to https://github.com/donereports/Done-Reports-API

--- api/models/link_user_group.rb
+++
@@ -1,6 +0,0 @@
-class GroupUser
-  include DataMapper::Resource
-  belongs_to :user, :key => true
-  belongs_to :group, :key => true
-  property :is_admin, Boolean, :default => false
-end
--- api/models/link_user_org.rb
+++
@@ -1,6 +0,0 @@
-class OrgUser
-  include DataMapper::Resource
-  belongs_to :user, :key => true
-  belongs_to :org, :key => true
-  property :is_admin, Boolean, :default => false
-end
