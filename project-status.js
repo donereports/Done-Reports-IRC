2ef6f067be9a4c3f718b9443c1c398b5622b1725
Aaron Parecki <aaron@parecki.com>
2013-08-16 12:08:59 -0700

    update to support submitting from other groups

--- project-status.js
+++ project-status.js
@@ -238,6 +238,8 @@ ProjectStatus.prototype.record_response = function(username, type, message, nick
         self.zen.send_privmsg(channel, "Sorry, I couldn't find an account for " + response.error_username);
       } else if(response.error == "user_not_in_group") {
         self.zen.send_privmsg(channel, "Sorry, you're not in this group!");
+      } else if(response.error == "user_not_in_org") {
+        self.zen.send_privmsg(channel, "Sorry, you're not in this organization!");
       } else {
         self.zen.send_privmsg(channel, "Something went wrong trying to store your entry!");
         console.log(response);
